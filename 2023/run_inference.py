91c9be2b5c4a77c0bee65a900da4afd3c13c8dd4
gminipark <pgm1219@khu.ac.kr>
2024-02-29 12:10:15 +0900

    update code for integration

--- 2023/run_inference.py
+++ 2023/run_inference.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 from tqdm import tqdm
+import json
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
@@ -72,6 +73,8 @@ def add_args(parser):
     parser.add_argument("--image_dir", type=str, required=True)
     parser.add_argument("--prompt_type", default='0', choices=['0', '1', '2'])
     parser.add_argument("--decoding_strategy",default='beam', choices=['greedy', 'beam', 'constrastive', 'diverse', 'sample'])
+    parser.add_argument("--input_path", type=str, default="../../Integration-Outputs/output_KAIST.json", required=True)
+    parser.add_argument("--output_path", type=str, default="../../Integration-Outputs/output_KHU.json")
     
     return parser
 
@@ -87,14 +90,10 @@ def main():
     processor = get_processor(args)
     
     # unanswerable question and uncertain_information(object)
-    samples = [
-        {"question" : "Why did Dokyung go to the old man?",
-         "uncertain_information" : [{"['old','man']" : False}],
-         "vid" : "AnotherMissOh17_001_0000"
-        },
-    ]
+    with open(args.input_path, 'r') as f:
+        loaded_json = json.load(f)
     
-    examples = convert_inputs_to_examples(samples)
+    examples = convert_inputs_to_examples(loaded_json)
     
     test_dataset = ImageTextGenerationDataset(args, examples, processor)
     test_dataloader = DataLoader(test_dataset, shuffle=False, batch_size=1, collate_fn=test_dataset.collate_fn)
@@ -104,9 +103,21 @@ def main():
     model.to(device)
     model.eval()
 
-    generated_results = []
-    
-    for batch in tqdm(test_dataloader):
+    generated_questions = []
+    results = []
+    example_qid = examples[0]['qid']
+
+    for i, batch in enumerate(tqdm(test_dataloader)):
+        if example_qid != examples[i]['qid']:
+            output = {  
+                    'qid': examples[i-1]['qid'],
+                    'vid': examples[i-1]['vid'],
+                    'main_question': examples[i-1]['question'],
+                    'sub_questions': generated_questions,
+                }
+            results.append(output)
+            generated_questions = []
+        example_qid = examples[i]['qid']
         
         inputs = move_to(batch, device)
         
@@ -118,9 +129,9 @@ def main():
         
         additional_questions = processor.batch_decode(outputs, skip_special_tokens=True)
         
-        generated_results += additional_questions
+        generated_questions += additional_questions
         
-    print(generated_results)
+    json.dump(results, open(args.output_path, 'w'), indent=4)
     
 if __name__ == "__main__":
     
--- 2023/utils.py
+++ 2023/utils.py
@@ -5,8 +5,8 @@ import ast
 import numpy as np
 from tqdm import tqdm
 
-
 def set_random_seed(seed):
+    
     torch.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
@@ -91,11 +91,12 @@ def convert_inputs_to_examples(inputs_list):
     
     examples = []
     for inputs in inputs_list:
-        ambiguous_objects = get_object_name_from_inputs(inputs['uncertain_information'])
+        ambiguous_objects = get_object_name_from_inputs(inputs['prediction'])
         for object_name in ambiguous_objects:
             example = {}
             example['ambiguous_object'] = object_name
             example['vid'] = inputs['vid']
+            example['qid'] = inputs['qid']
             example['question'] = inputs['question']
             examples.append(example)
     
