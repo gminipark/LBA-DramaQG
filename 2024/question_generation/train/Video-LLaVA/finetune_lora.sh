8f328cb74bf0b21ecdde9a15668ac349b0c74212
gminipark <pgm1219@khu.ac.kr>
2024-11-10 11:54:54 +0000

    add shell file for lora finetuning

---
+++ 2024/question_generation/train/Video-LLaVA/finetune_lora.sh
@@ -0,0 +1,45 @@
+
+
+JSON_FOLDER="data"
+IMAGE_FOLDER="data/AnotherMissOh_images/dramaqa_frames"
+VIDEO_FOLDER="data/AnotherMissOh_images/dramaqa_frames"
+cd .
+
+HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=0 deepspeed videollava/train/train_mem.py \
+    --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
+    --deepspeed ./scripts/zero2_offload.json \
+    --model_name_or_path lmsys/vicuna-7b-v1.5 \
+    --version v1 \
+    --data_path ${JSON_FOLDER}/llava_dramaqg_train.json \
+    --image_folder ${IMAGE_FOLDER} \
+    --image_tower LanguageBind/LanguageBind_Image \
+    --video_folder ${VIDEO_FOLDER} \
+    --video_tower LanguageBind/LanguageBind_Video_merge \
+    --mm_projector_type mlp2x_gelu \
+    --pretrain_mm_mlp_adapter ./checkpoints/videollava-7b-pretrain/mm_projector.bin \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
+    --bf16 True \
+    --output_dir ./checkpoints/videollava-7b-lora-epoch-front \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "epoch" \
+    --num_train_epochs 5 \
+    --save_total_limit 5 \
+    --learning_rate 2e-4 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 100 \
+    --tf32 True \
+    --model_max_length 2048  --tokenizer_model_max_length 3072 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 8 \
+    --lazy_preprocess True \
+    --report_to tensorboard \
+    --cache_dir "./cache_dir"
