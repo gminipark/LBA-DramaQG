911d939e0c46cfeefc2fe1467b0793b2da0b3c53
gminipark <pgm1219@khu.ac.kr>
2024-11-10 11:53:58 +0000

    add shell file for merging

---
+++ 2024/question_generation/train/Video-LLaVA/merge.sh
@@ -0,0 +1,4 @@
+python merge_lora_weights.py \
+    --model-path checkpoints/videollava-7b-lora-epoch/checkpoint-565 \
+    --model-base lmsys/vicuna-7b-v1.5 \
+    --save-model-path checkpoints/videollava-7b-checkpoint-e1-merged \
